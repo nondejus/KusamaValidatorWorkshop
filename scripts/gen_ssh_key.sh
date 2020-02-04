3e56de958144d82c52d60c235e544227e7f798ea
wpank <9498646+wpank@users.noreply.github.com>
2020-02-04 11:51:35 +0100

    modified ssh generating script

--- scripts/gen_ssh_key.sh
+++ scripts/gen_ssh_key.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
+# Generates an ssh key in the .ssh folder in the root directory of the project
 
-mkdir -p $PWD/.ssh
-# ssh-keygen -t rsa -f $PWD/../.ssh/id_rsa -C $USER
-ssh-keygen -f $PWD/../.ssh/id_rsa
+mkdir -p /../.ssh
+ssh-keygen -t rsa -f /../.ssh/id_rsa -C $USER
